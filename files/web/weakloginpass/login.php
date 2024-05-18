39bc19baad53bb651b4fac1634e01551ac8b23da
prince <princekrvert@gmail.com>
2024-05-18 12:22:25 +0530

    Weak login password challange added

---
+++ files/web/weakloginpass/login.php
@@ -0,0 +1,11 @@
+<?php
+
+if ($_POST['username'] == "admin" && $_POST['password'] == "football" ){
+	echo ("this is the flag ");
+        header('Location: https://github.com/princekrvert/general/blob/main/linuxenm/ctf/loginpass');
+        exit();
+}else {
+        header('Location: https://github.com/princekrvert/');
+        exit();
+}
+
